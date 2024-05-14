7fb403816163fc906c433057df1c704cbe529cc0
obaydul_Kuasha <obaydul00000@gmail.com>
2024-05-14 18:36:01 +0600

    Create my react.js project on Youtube clone and some work then update git hub

---
+++ src/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <>
+            <div className='flex  '>
+                {/* this is left Menu icon and youtube logo */}
+                <div>
+                    <h1>m</h1>
+                    <img src="" alt="" />
+                </div>
+                {/* middel side part search button  */}
+                <div>
+                    <input type="text" name="input" id="" />
+                    <h1>Serch</h1>
+                </div>
+                {/* look this end part of youtube */}
+                <div>
+                    <h1>vi</h1>
+                    <h1>appi</h1>
+                    <h1>noti</h1>
+                    <h1>avate</h1>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
+
+
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBp4_418Q1L_AdUnuqcrskS-8Fw0224PEM",
+    authDomain: "clone-636b8.firebaseapp.com",
+    projectId: "clone-636b8",
+    storageBucket: "clone-636b8.appspot.com",
+    messagingSenderId: "108776067919",
+    appId: "1:108776067919:web:3eed7289f6a745ab36eb3a",
+    measurementId: "G-YF7V6RNMQZ"
+  };
